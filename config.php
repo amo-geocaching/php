abd52297f5949d59a74f7333b46d67bb8b0eb8e1
Andy van Sundert <andy482382@gmail.com>
2019-08-27 14:58:46 +0200

    added config file for Database en logincontroller
    added code for login and register

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sem40
+ * Date: 15/04/2019
+ * Time: 09:27
+ */
+
+$dbHost = "localhost";
+$dbName = "";
+$dbUser = "root";
+$dbPass = "";
+
+$db = new PDO(
+    "mysql:host=$dbHost;dbname=$dbName",
+    $dbUser,
+    $dbPass
+);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+session_start();
