8b302d5279451b3f2d4826041bc3fbc9a215ec52
Andy van Sundert <andy482382@gmail.com>
2019-08-28 16:49:29 +0200

    added style sheet and a style font

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require 'header.php';
+?>
+<div class="container">
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href=""></a>
+    </div>
+</div>
+<?php
+require 'footer.php';
+?>
