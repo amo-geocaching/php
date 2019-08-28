e6686941933ac918c56e8b245fc9567649547581
Sem van Elteren <sem407@outlook.com>
2019-08-28 16:42:33 +0200

    added the google maps page

---
+++ maps.php
@@ -0,0 +1,32 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>maps</title>
+</head>
+<body>
+        <h1>Maps</h1>
+
+        <div id="googleMap" style="width:100%;height:600px;"></div>
+
+        <script>
+            function myMap() {
+                var mapProp= {
+                    center:new google.maps.LatLng(51.508742,-0.120850),
+                    zoom:5,
+                };
+                var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            }
+        </script>
+
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBkT7PLWK2NvHi-oPt7iPf-wLRix53NUnQ&callback=myMap"></script>
+</body>
+
+
+</html>
