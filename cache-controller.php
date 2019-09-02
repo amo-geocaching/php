36fb3ffc00a26d0674f4f4a56e8532ba81d2e050
Evilman <timo02@me.com>
2019-09-02 12:44:40 +0200

    Can add rating and commentary

--- cache-controller.php
+++ cache-controller.php
@@ -23,6 +23,25 @@ if(isset($_POST['logcache'])){
         'userid'    => $userid,
         'logdate'   => $logdate
     ]);
+    header("Location: cache-detail.php?=$cacheid");
+}
+else if(isset($_POST['rating']) && $_POST['rating'] <= 5 && $_POST['rating'] >= 0){
+    $sql = "UPDATE logs SET rating = :rating WHERE cacheid = :cacheid";
+    $prepare = $db->prepare($sql);
+    $prepare->execute([
+        ':rating'   => $_POST['rating'],
+        ':cacheid'  => $cacheid
+    ]);
+    header("Location: cache-detail.php?=$cacheid");
+}
+else if(isset($_POST['comment']) && strlen($_POST['comment']) <= 500){
+    $sql = "UPDATE logs SET comment = :comment WHERE cacheid = :cacheid";
+    $prepare = $db->prepare($sql);
+    $prepare->execute([
+        ':comment'   => $_POST['comment'],
+        ':cacheid'  => $cacheid
+    ]);
+    header("Location: cache-detail.php?=$cacheid");
 }
 else{
     echo 'Er is een fout opgetreden';
