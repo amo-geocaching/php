132a9821996384c20a17578e1ea4795a4edfff6e
Nino <ninodehaan@gmail.com>
2019-09-02 11:09:19 +0200

    Added Informatin Page
    Informatie pagina is aangemaakt en tekst er in toegevoegd

---
+++ informatie.php
@@ -0,0 +1,23 @@
+<?php
+require 'header.php';
+
+?>
+
+<div class="container">
+    <div class="informatie">
+        <h1>Informatie</h1>
+        <div class="locaties-info">
+            <h2>Locaties</h2>
+            <p>Geocaching word gedaan op verschillende locaties in Breda. Dit kan ook nog verschillen kwa wat voor terrein
+                je de geocache zou kunnen vinden. Het kan boven de grond, onder de grond en hogerop liggen dus let op!</p>
+        </div>
+        <div class="vinden-info">
+            <p>Geocaches kun je op verschillende plekken vinden, zoals in de grond. Hiervoor moet je voorbereid zijn
+            want het kan dus zomaar dat je moet graven om de geocache te vinden. Dit hangt af aan de moeilijkheids graad.
+            Sommige caches kunnen ook hogerop zitten bijvoorbeeld in een boom dus je moet niet bang zijn om vies te worden
+            en te moeten klimmen.</p>
+        </div>
+    </div>
+</div>
+
+<?php require 'footer.php';
