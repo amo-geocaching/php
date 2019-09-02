f54076df90816b243beb8c5ca8c75d24ade27653
Andy van Sundert <andy482382@gmail.com>
2019-09-02 09:07:47 +0200

    removed unnecessary lines

--- maps.php
+++ maps.php
@@ -1,17 +1,3 @@
-<?php
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>maps</title>
-</head>
-<body>
-
 <?php require 'header.php'?>
 <main>
     <h1>Maps</h1>
@@ -32,7 +18,3 @@
 </main>
 <?php require 'footer.php'?>
 
-</body>
-
-
-</html>
