0495af751f355965f65dfa5bae85acbad4d477eb
Evilman <timo02@me.com>
2019-09-04 17:06:02 +0200

    Map now shows caches stored in database

--- maps.php
+++ maps.php
@@ -1,4 +1,12 @@
-<?php require 'header.php'?>
+<?php
+require 'config.php';
+$sql = "SELECT * FROM caches";
+$query = $db->query($sql);
+$caches = $query->fetchAll(PDO:: FETCH_ASSOC);
+$length = count($caches);
+
+require 'header.php';
+?>
 <main>
     <h1>Maps</h1>
 
@@ -6,11 +14,35 @@
 
     <script>
         function myMap() {
-            var mapProp= {
+            let mapProp= {
                 center:new google.maps.LatLng(51.508742,-0.120850),
                 zoom:5,
             };
-            var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+            let length = <?php echo $length?>;
+
+            <?php
+            $js_caches = json_encode($caches);
+            echo "let jscaches = ". $js_caches . ";\n";
+            ?>
+            let marker = [];
+
+            for (let i = 0; i < jscaches.length; i++) {
+                let latitude = parseFloat(jscaches[i].coordinateY);
+                let longitude = parseFloat(jscaches[i].coordinateX);
+                let myLatLng = {lat: latitude, lng: longitude};
+                marker[i] = new google.maps.Marker({
+                    position: myLatLng,
+                    map: map,
+                    description: jscaches[i].description,
+                    title: jscaches[i].cachename,
+                    id: i
+
+                });
+
+            }
+
         }
     </script>
 
