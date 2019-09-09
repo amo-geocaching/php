4b1bd8eb0b3b1cd8c0051b6a309fab74c649bb93
Andy van Sundert <andy482382@gmail.com>
2019-09-09 10:52:38 +0200

    fixed issue with a link

--- cache-list.php
+++ cache-list.php
@@ -12,7 +12,7 @@ $caches = $query->fetchAll(PDO::FETCH_ASSOC);
 foreach ($caches as $cache){
 
     $cachename = $cache['cachename'];
-    echo "<li> <a href='cache-detail.php?id={$cache ['cacheid']}'>$cachename</a></li>";
+    echo "<li> <a href='cache-detail.php?cacheid={$cache ['cacheid']}'>$cachename</a></li>";
 
 }
 ?>
