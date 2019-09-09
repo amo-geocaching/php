4b2eee9101c6b4df6979463dd7460ce0d6c0f896
Andy van Sundert <andy482382@gmail.com>
2019-09-09 10:29:49 +0200

    added cache list

--- cache-detail.php
+++ cache-detail.php
@@ -7,36 +7,57 @@
  */
 
 require 'header.php';
-if($_SESSION['loggedin'] == true){
-    $_SESSION['cacheid'] = $_GET['cacheid'];
+if($_SESSION['loggedin'] == true) {
+$_SESSION['cacheid'] = $_GET['cacheid'];
 
-    $sql = "SELECT * FROM caches WHERE cacheid = :cacheid";
-    $prepare = $db->prepare($sql);
-    $prepare->execute([
-        ':cacheid' => $_GET['cacheid']
-    ]);
-    $caches = $prepare->fetch(PDO::FETCH_ASSOC);
+$sql = "SELECT * FROM caches WHERE cacheid = :cacheid";
+$prepare = $db->prepare($sql);
+$prepare->execute([
+    ':cacheid' => $_GET['cacheid']
+]);
+$caches = $prepare->fetch(PDO::FETCH_ASSOC);
 
-    $sql = "SELECT * FROM logs WHERE userid = :userid AND cacheid = :cacheid";
-    $prepare = $db->prepare($sql);
-    $prepare->execute([
-        ':userid' => $_SESSION['id'],
-        ':cacheid' => $_GET['cacheid']
-    ]);
-    $cacheinfo = $prepare->fetch(PDO::FETCH_ASSOC);
-    $logdate = $cacheinfo['logdate'];
-    ?>
-    <main>
+$sql = "SELECT * FROM logs WHERE userid = :userid AND cacheid = :cacheid";
+$prepare = $db->prepare($sql);
+$prepare->execute([
+    ':userid' => $_SESSION['id'],
+    ':cacheid' => $_GET['cacheid']
+]);
+$cacheinfo = $prepare->fetch(PDO::FETCH_ASSOC);
+$logdate = $cacheinfo['logdate'];
+?>
+<main>
+    <?php
+    if ($cacheinfo == false) {
+        ?>
+        <form action="cache-controller.php" method="post">
+            <input type="hidden" name="logcache" value="1">
+            <input id="submit" type="submit" value="Log">
+        </form>
         <?php
-        if(isset($caches['cacheid'])){
-            if ($cacheinfo == false){?>
+    } else {
+        echo "<h3>Je hebt deze cache gelogt op $logdate</h3>";
+        ?>
+        <form action="cache-controller.php" method="post">
+            <label for="rating">Rating</label>
+            <input type="number" min="0" max="5" name="rating">
+            <input id="submit" type="submit" value="log">
+        </form>
+        <form action="cache-controller.php" method="post">
+            <label for="comment">Comment</label>
+            <input type="text" maxlength="500" name="comment">
+            <input id="submit" type="submit" value="Log">
+        </form>
+        <?php
+        if (isset($caches['cacheid'])) {
+            if ($cacheinfo == false) {
+                ?>
                 <form action="controllers/cache-controller.php" method="post">
                     <input type="hidden" name="logcache" value="1">
                     <input id="submit" type="submit" value="Log">
                 </form>
                 <?php
-            }
-            else {
+            } else {
                 echo "<h3>Je hebt deze cache gelogt op $logdate</h3>";
                 ?>
                 <form action="controllers/cache-controller.php" method="post">
@@ -49,15 +70,12 @@ if($_SESSION['loggedin'] == true){
                 </form>
                 <?php
             }
-        }else{
+        } else if ($_SESSION['loggedin'] == false) {
+            echo 'Je moet eerst inloggen om caches te kunnen zien';
+        } else {
             echo 'Deze cache bestaat niet!';
-        }?>
-
-
-
-    </main>
-
-<?php }else if($_SESSION['loggedin'] == false){
-    echo 'Je moet eerst inloggen om caches te kunnen zien';
-}
-require 'footer.php';
+        }
+        require 'footer.php';
+    }
+    }
+    ?>
--- cache-list.php
+++ cache-list.php
@@ -1,6 +1,22 @@
 <?php
 require 'header.php';
+
+$sql = "SELECT * FROM caches";
+$query = $db->query($sql);
+$caches = $query->fetchAll(PDO::FETCH_ASSOC);
+
+
 ?>
+<ul>
+<?php
+foreach ($caches as $cache){
+
+    $cachename = $cache['cachename'];
+    echo "<li> <a href='cache-detail.php?id={$cache ['cacheid']}'>$cachename</a></li>";
+
+}
+?>
+</ul>
 
 
 
