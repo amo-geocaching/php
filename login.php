4a1ece45e9bf3040fc7a65babae561671521157a
Andy van Sundert <andy482382@gmail.com>
2019-09-10 16:25:02 +0200

    added css to login form

--- login.php
+++ login.php
@@ -8,13 +8,13 @@ require 'header.php'
                 <h3>Login</h3>
             </div>
             <form id="login-account-form" action="controllers/logincontroller.php" method="post">
-                <label for="username">Gebruikersnaam</label>
+                <label class="UserLabel" for="username">Gebruikersnaam</label>
                 <input type="text" name="username" required="" class="username">
-                <label for="password">Wachtwoord</label>
+                <label class="PassLabel" for="password">Wachtwoord</label>
                 <input type="password" name="password" required="" class="password">
-                <input id="submit-team" type="submit" name="type" value="login">
+                <input class="loginbutton"  id="submit-team" type="submit" name="type" value="login">
             </form>
-            <button id="forgot" onclick="window.location.href = 'submit-email.php';">Vergeten</button>
+            <button class="forgotbutton"  id="forgot" onclick="window.location.href = 'submit-email.php';">Wachtwoord vergeten?</button>
         </div>
     </div>
 </div>
