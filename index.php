e63aeb01b67b87a4ffd4069c12d804a0b66dec98
Evilman <timo02@me.com>
2019-09-11 16:59:30 +0200

    improved css styling

--- index.php
+++ index.php
@@ -2,7 +2,22 @@
 require 'header.php';
 ?>
 
+<main>
+    <div class="detail-main">
+        <div class="side">
 
+        </div>
+        <div class="middle">
+
+            
+
+
+        </div>
+        <div class="side">
+
+        </div>
+    </div>
+</main>
 
 <?php
 require 'footer.php';
