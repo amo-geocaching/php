40f9e7f3d59fb594d81f84ac1b7a6934e32014f4
Sem van Elteren <sem407@outlook.com>
2019-09-12 11:35:13 +0200

    removed merge text

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ require 'header.php';
 ?>
 <div class="container">
 
-<<<<<<< HEAD
+
 </div>
-=======
+
 <main>
     <div class="detail-main">
         <div class="side">
@@ -23,7 +23,6 @@ require 'header.php';
     </div>
 </main>
 
->>>>>>> master
 <?php
 require 'footer.php';
 ?>
