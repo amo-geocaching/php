508b9178bd5c994e67c6fd309f29945965ca5c0e
Nino <ninodehaan@gmail.com>
2019-09-17 13:38:58 +0200

    Home page text

--- index.php
+++ index.php
@@ -7,8 +7,13 @@ require 'header.php';
 
         </div>
         <div class="middle">
-
-
+            <h1>Geocaches</h1>
+                <p>Bij Geocachen moet je via een map een cache kiezen. Vervolgens moet je die cache vinden met behulp van een gps
+                of via de map die je hier op de website ziet, caches kunnen overal zitten dus wees voorbereid op avontuur!
+                Je kan zien bij omshrijvingen wat je er nodig voor zal hebben om de cache te kunnen vinden. Je kan nadat je een cache
+                ge-'logged' heb de cache een rating geven en feedback over de ervaring van de cache. Veel speel plezier
+                ik ga nu zelf zoals goede ouwe Bilbo Balings ooit zei 'I'm going on an adventure'!</p>
+            <img src="img/geocache.png" alt="">
 
 
         </div>
