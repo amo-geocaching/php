440107fc78d5563e5f198c3de82cd16b157e0eec
Evilman <timo02@me.com>
2019-09-18 17:11:49 +0200

    Fixed css on edit pages

--- edit-account.php
+++ edit-account.php
@@ -14,23 +14,43 @@ if(isset($result)){
 
 require 'header.php';?>
 <main>
-    <div class="main-form">
-        <div class="create-form-background">
-                <div class="title-form">
-                <h3>Account aanpassen</h3>
-            </div>
-            <form id="create-account-form" action="controllers/newaccount-controller.php" method="post">
-                <label for="name">Nieuwe Gebruikersnaam</label>
-                <input type="text" name="username" required="">
-                <label for="players">Nieuwe Wachtwoord</label>
-                <input type="password" name="password" required="">
-                <label for="players">Wachtwoord Confirm</label>
-                <input type="password" name="passwordConfirm" required="">
-                <input id="submit" type="submit" name="type" value="register">
-            </form>
-        </div>
+    <div class="detail-main">
+        <div class="side">
 
+        </div>
+        <div class="middle">
+            <div class="main-form">
+                <div class="create-form-background">
+                    <div class="title-form">
+                        <h3>Account aanpassen</h3>
+                    </div>
+                    <form class="login-register" id="create-account-form" action="controllers/newaccount-controller.php" method="post">
+                        <div class="form-groups">
+                            <div class="form-group">
+                                <label for="name">Nieuwe Gebruikersnaam:</label>
+                                <input type="text" name="username" required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="players">Nieuwe Wachtwoord:</label>
+                                <input type="password" name="password" required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="players">Wachtwoord Confirm:</label>
+                                <input type="password" name="passwordConfirm" required="">
+                            </div>
+
+                        </div>
+                        <input id="submit" type="hidden" name="type" value="edit">
+                        <input class="editbutton" type="submit" id="submit" value="Edit">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="side">
+
+        </div>
     </div>
+
 </main>
 <?php require 'footer.php';
 }?>
--- submit-email.php
+++ submit-email.php
@@ -8,15 +8,24 @@
 require 'header.php'
 ?>
     <main>
-        <div class="main-edit">
-            <h2>Vul hieronder je email in om je account aan te passen.</h2>
-            <form id="login-account-form" action="controllers/edit-controller.php" method="post">
-                <label for="name">Email</label>
-                <input type="email" name="email" required="">
-                <input id="submit-email" type="submit" value="edit">
-            </form>
-        </div>
+        <div class="detail-main">
+            <div class="side">
 
+            </div>
+            <div class="middle">
+                <div class="main-edit">
+                    <h2>Vul hieronder je email in om je account aan te passen.</h2>
+                    <form id="login-account-form" action="controllers/edit-controller.php" method="post">
+                        <label for="name">Email</label>
+                        <input type="email" name="email" required="">
+                        <input id="submit-email" type="submit" value="edit">
+                    </form>
+                </div>
+            </div>
+            <div class="side">
+
+            </div>
+        </div>
     </main>
 
 <?php require 'footer.php' ?>
