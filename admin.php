73f916c3ead80d52f63801622f0591525fc58a50
Evilman <timo02@me.com>
2019-09-18 16:36:29 +0200

    Fixed CSS on admin.php

--- admin.php
+++ admin.php
@@ -6,36 +6,41 @@ require 'header.php';
 
     </div>
     <div class="middle">
-        <div class="add-cache" >
-            <form action="controllers/create-cachecontroller.php" method="post">
+        <div class="main-form" >
+            <form action="controllers/create-cachecontroller.php" class="login-register" method="post">
                 <input type="hidden" name="type" value="createcache">
-                <div class="form-group">
-                    <label for="cachename">Cache Naam</label>
-                    <input type="text" name="cachename" id="cachename">
-
-                    <label for="description">Omschrijving</label>
-                    <input type="text" name="description" id="description">
-
-                    <label for="coordinateX">Breedtegraad</label>
-                    <input type="text" name="coordinateX" id="coordinateX">
-
-                    <label for="coordinateY">Lengtegraad</label>
-                    <input type="text" name="coordinateY" id="coordinateY">
-
-                    <label for="difficulty">Moeilijkheid</label>
-                    <input type="text" name="difficulty" id="difficulty">
-
-                    <label for="rating">Rating</label>
-                    <input type="text" name="rating" id="rating">
-
-                    <label for="properties">Eigenschappen</label>
-                    <input type="text" name="properties" id="properties">
-
-                    <label for="tip">Tips</label>
-                    <input type="text" name="tip" id="tip">
+                <div class="form-groups">
+                    <div class="form-group">
+                        <label for="cachename">Cache Naam:</label>
+                        <input type="text" name="cachename" id="cachename">
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Omschrijving:</label>
+                        <input type="text" name="description" id="description">
+                    </div>
+                    <div class="form-group">
+                        <label for="coordinateX">Breedtegraad:</label>
+                        <input type="text" name="coordinateX" id="coordinateX">
+                    </div>
+                    <div class="form-group">
+                        <label for="coordinateY">Lengtegraad:</label>
+                        <input type="text" name="coordinateY" id="coordinateY">
+                    </div>
+                    <div class="form-group">
+                        <label for="difficulty">Moeilijkheid:</label>
+                        <input type="text" name="difficulty" id="difficulty">
+                    </div>
+                    <div class="form-group">
+                        <label for="properties">Eigenschappen:</label>
+                        <input type="text" name="properties" id="properties">
+                    </div>
+                    <div class="form-group">
+                        <label for="tip">Tips:</label>
+                        <input type="text" name="tip" id="tip">
+                    </div>
                 </div>
 
-                <input type="submit" value="Add Cache">
+                <input class="add-cache-button" type="submit" value="Add Cache">
             </form>
         </div>
     </div>
